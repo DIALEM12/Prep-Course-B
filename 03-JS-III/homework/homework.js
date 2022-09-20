da48bf003951081cbf010a5fea223e2c11360505
diedo malagon <dialem12@outlook.com>
2022-09-19 21:11:36 -0500

    homework3

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,57 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var numeros = [1,2,3,4,5,6,7,8,9,10];
+    var suma = numeros.reduce(function(a, b){
+      return a + b;
+    })
+    return suma
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares=[];
+
+  for (var i = 0;i < array.length; i++){
+    if (array[i] % 2 === 0){
+      pares.push(array[i]);
+    }
+  }
+  return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var cuadrado=[];
+
+  for (var i = 0;i < array.length; i++){
+    {
+      cuadrado.push(array[i]**2);
+    }
+  }
+  return cuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var acumulador = 0;
+  for(var i=0;i < array.length; i++){
+    acumulador=acumulador+ array[i];
+  }
+  return acumulador
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length
+  
 }
   
   
